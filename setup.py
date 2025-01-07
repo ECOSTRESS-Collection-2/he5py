fb1445d001fcf8da17e836c3e67f8e4e7f9de8be
Gregory Halverson <gregory.halverson@gmail.com>
2025-01-07 13:04:29 -0800

    trying to revert to the ECOSTRESS collection 2 way of installing C++

--- setup.py
+++ setup.py
@@ -1,26 +1,33 @@
-from skbuild import setup
+from setuptools import setup, find_packages
+
+try:
+    import pybind11_cmake
+except ImportError:
+    print("pybind11-cmake must be installed."
+          "Try \n \t pip install pybind11_cmake")
+    import sys
+    sys.exit()
+
+from pybind11_cmake import CMakeExtension, CMakeBuild
+
+from os.path import join, abspath, dirname
+
+__author__ = "Gregory Halverson"
+AUTHOR_EMAIL = "Gregory.H.Halverson@jpl.nasa.gov"
+
+def version():
+    with open(join(abspath(dirname(__file__)), "he5py", "version.txt"), "r") as file:
+        return file.read()
 
 setup(
-    name="he5py",
-    version="1.0.0",
-    description="Utilities for working with the HDF-EOS-5 remote sensing data format",
-    author="Gregory H. Halverson",
-    license="Apache Software License",
-    packages=["he5py"],
-    cmake_install_dir="he5py",
-    include_package_data=True,
-    install_requires=[
-        "affine",
-        "colored-logging",
-        "numpy",
-        "h5py",
-        "rasters"
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: C++",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
+    version=version(),
+    author=__author__,
+    author_email=AUTHOR_EMAIL,
+    long_description='',
+    setup_requires=['pybind11_cmake'],
+    ext_modules=[CMakeExtension('HE5PY_CPP')],
+    cmdclass=dict(build_ext=CMakeBuild),
+    zip_safe=False,
+    packages=find_packages() + ["he5py", "he5py.src"],
+    package_data={'': ["*", "he5py/*", "he5py/src/*"]}
 )
